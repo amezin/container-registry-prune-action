4b4965427da5aa037005aa1849921efda619eaa9
Aleksandr Mezin <mezin.alexander@gmail.com>
2026-09-17 02:39:39 +0300

    v0.4.5

--- dist/index.js
+++ dist/index.js
@@ -51211,7 +51211,7 @@ function octokit_getOctokit(token, options, ...additionalPlugins) {
 }
 
 ;// CONCATENATED MODULE: ./package.json
-const package_namespaceObject = /*#__PURE__*/JSON.parse('{"UU":"@amezin/container-registry-prune-action","rE":"0.4.4"}');
+const package_namespaceObject = /*#__PURE__*/JSON.parse('{"UU":"@amezin/container-registry-prune-action","rE":"0.4.5"}');
 ;// CONCATENATED MODULE: ./src/main.ts
 
 
